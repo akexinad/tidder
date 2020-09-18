e25c73a97224b8a1d0c0aae2bab1ab65d46d5629
danny <danixeka@gmail.com>
2020-09-18 17:26:34 +1000

    created login and navbar

---
+++ tidder-client/src/pages/login.tsx
@@ -0,0 +1,62 @@
+import React, { FC } from "react";
+import { Form, Formik } from "formik";
+import { Box, Button } from "@chakra-ui/core";
+import { useRouter } from "next/router";
+
+import { useLoginMutation } from "../generated/graphql";
+
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { toErrorMap } from "../utils/toErrorMap";
+
+export const Login: FC<{}> = ({}) => {
+    const router = useRouter();
+    const [, login] = useLoginMutation();
+
+    return (
+        <Wrapper variant="small">
+            <Formik
+                initialValues={{ username: "", password: "" }}
+                onSubmit={async (values, { setErrors }) => {
+                    const response = await login({ options: values });
+
+                    if (response.data.login.errors) {
+                        setErrors(toErrorMap(response.data.login.errors));
+                    } else if (response.data.login.user) {
+                        router.push("/");
+                    }
+
+                    response.data.login.user.id;
+                }}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <InputField
+                            name="username"
+                            placeholder="username"
+                            label="Username"
+                        />
+                        <Box mt={4}>
+                            <InputField
+                                name="password"
+                                placeholder="password"
+                                label="Password"
+                                type="password"
+                            />
+                        </Box>
+                        <Button
+                            mt={4}
+                            type="submit"
+                            variantColor="teal"
+                            isLoading={isSubmitting}
+                        >
+                            Login
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+    );
+};
+
+export default Login;
