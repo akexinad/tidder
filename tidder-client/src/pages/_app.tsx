4cf869a287abbbff78a3cdcbca5d65647703b8cd
danny <danixeka@gmail.com>
2020-09-18 17:46:59 +1000

    updating cache and graphql fragments

--- tidder-client/src/pages/_app.tsx
+++ tidder-client/src/pages/_app.tsx
@@ -1,15 +1,76 @@
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
-import { Provider, createClient } from "urql";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
+import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from "../generated/graphql";
+
+import theme from "../theme";
+
+function betterUpdateQuery<Result, Query>(
+    cache: Cache,
+    qi: QueryInput,
+    result: any,
+    fn: (r: Result, q: Query) => Query
+) {
+    return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
     url: "http://localhost:4000/graphql",
     // this is important for cookie data
     fetchOptions: {
         credentials: "include"
-    }
-});
+    },
+    /**
+     * this block of code below will run everytime the login or register
+     * mutation runs and it's purpose is to update the cache.
+     * 
+     * In particular we are updating the me query and placing the correct
+     * user inside of there.
+     */
+    exchanges: [
+        dedupExchange,
+        cacheExchange({
+            updates: {
+                Mutation: {
+                    login: (_result, args, cache, info) => {
+                        betterUpdateQuery<LoginMutation, MeQuery>(
+                            cache,
+                            { query: MeDocument },
+                            _result,
+                            (result, query) => {
+                                if (result.login.errors) {
+                                    return query;
+                                } else {
+                                    return {
+                                        me: result.login.user
+                                    };
+                                }
+                            }
+                        );
+                    },
 
-import theme from "../theme";
+                    register: (_result, args, cache, info) => {
+                        betterUpdateQuery<RegisterMutation, MeQuery>(
+                            cache,
+                            { query: MeDocument },
+                            _result,
+                            (result, query) => {
+                                if (result.register.errors) {
+                                    return query;
+                                } else {
+                                    return {
+                                        me: result.register.user
+                                    };
+                                }
+                            }
+                        );
+                    }
+                }
+            }
+        }),
+        fetchExchange
+    ]
+});
 
 function MyApp({ Component, pageProps }) {
     return (
