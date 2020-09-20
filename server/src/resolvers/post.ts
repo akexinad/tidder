a64db93b967aad5397b78c578cf78f2f2fcfe319
turiano <danixeka@gmail.com>
2020-09-20 19:48:52 +1000

    adding authentication middleware

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,5 +1,28 @@
 import { Post } from "../entities/Post";
-import { Arg, Int, Mutation, Query, Resolver } from "type-graphql";
+import {
+    Arg,
+    Ctx,
+    Field,
+    InputType,
+    Int,
+    Mutation,
+    Query,
+    Resolver,
+    UseMiddleware
+} from "type-graphql";
+
+import { MyContext } from "src/types";
+
+import { isAuth } from "../middleware/isAuth";
+
+@InputType()
+class PostInput {
+    @Field()
+    title: string;
+
+    @Field()
+    text: string;
+}
 
 @Resolver()
 export class PostResolver {
@@ -16,8 +39,15 @@ export class PostResolver {
     }
 
     @Mutation(() => Post)
-    async createPost(@Arg("title", () => String) title: string): Promise<Post> {
-        return Post.create({ title }).save();
+    @UseMiddleware(isAuth)
+    async createPost(
+        @Arg("options") options: PostInput,
+        @Ctx() { req }: MyContext
+    ): Promise<Post> {
+        return Post.create({
+            ...options,
+            authorId: +req.session.userId
+        }).save();
     }
 
     @Mutation(() => Post, { nullable: true })
