3b9ce3c0e947c20cea900db94a2aa3243939d1e8
turiano <danixeka@gmail.com>
2020-09-21 14:28:00 +1000

    created wrapper variant type

--- client/src/components/Wrapper.tsx
+++ client/src/components/Wrapper.tsx
@@ -1,8 +1,10 @@
 import { Box } from "@chakra-ui/core";
 import React, { FC } from "react";
 
+export type WrapperVariant = "small" | "regular";
+
 interface WrapperProps {
-    variant?: "small" | "regular";
+    variant?: WrapperVariant;
 }
 
 export const Wrapper: FC<WrapperProps> = ({
