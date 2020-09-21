5b5fbd1f605dc85813ba6e29a46ece024ffb4770
turiano <danixeka@gmail.com>
2020-09-21 14:27:16 +1000

    added textarea input type

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -4,26 +4,30 @@ import {
     FormControl,
     FormLabel,
     Input,
-    FormErrorMessage
+    FormErrorMessage, Textarea
 } from "@chakra-ui/core";
 
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
     label: string;
     name: string;
+    textarea?: boolean;
 };
 
 export const InputField: FC<InputFieldProps> = ({
     label,
+    textarea = false,
     size: _,
     ...props
 }) => {
     const [field, { error }] = useField(props);
 
+    const InputElementType = textarea ? Textarea : Input;
+
     return (
         // the !! casts the object to a boolean
         <FormControl isInvalid={!!error}>
             <FormLabel htmlFor={field.name}>{label}</FormLabel>
-            <Input
+            <InputElementType
                 {...field}
                 {...props}
                 type={props.type}
