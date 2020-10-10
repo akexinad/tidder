12fc602753632665bb48146c1166b782d84b6629
akexinad <danixeka@gmail.com>
2020-10-10 19:24:26 +1100

    Create updatePost mutation for client side

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -67,8 +67,8 @@ export type Mutation = {
   __typename?: 'Mutation';
   vote: Scalars['Boolean'];
   createPost: Post;
-  editPost?: Maybe<Post>;
   deletePost: Scalars['Boolean'];
+  updatePost?: Maybe<Post>;
   changePassword: UserResponse;
   forgotPassword: Scalars['Boolean'];
   register: UserResponse;
@@ -88,13 +88,14 @@ export type MutationCreatePostArgs = {
 };
 
 
-export type MutationEditPostArgs = {
-  title?: Maybe<Scalars['String']>;
-  id: Scalars['Float'];
+export type MutationDeletePostArgs = {
+  id: Scalars['Int'];
 };
 
 
-export type MutationDeletePostArgs = {
+export type MutationUpdatePostArgs = {
+  text: Scalars['String'];
+  title: Scalars['String'];
   id: Scalars['Int'];
 };
 
@@ -256,6 +257,21 @@ export type RegisterMutation = (
     { __typename?: 'UserResponse' }
     & RegularUserResponseFragment
   ) }
+);
+
+export type UpdatePostMutationVariables = Exact<{
+  id: Scalars['Int'];
+  title: Scalars['String'];
+  text: Scalars['String'];
+}>;
+
+
+export type UpdatePostMutation = (
+  { __typename?: 'Mutation' }
+  & { updatePost?: Maybe<(
+    { __typename?: 'Post' }
+    & Pick<Post, 'id' | 'title' | 'text' | 'textSnippet'>
+  )> }
 );
 
 export type VoteMutationVariables = Exact<{
@@ -430,6 +446,20 @@ export const RegisterDocument = gql`
 
 export function useRegisterMutation() {
   return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
+};
+export const UpdatePostDocument = gql`
+    mutation UpdatePost($id: Int!, $title: String!, $text: String!) {
+  updatePost(id: $id, title: $title, text: $text) {
+    id
+    title
+    text
+    textSnippet
+  }
+}
+    `;
+
+export function useUpdatePostMutation() {
+  return Urql.useMutation<UpdatePostMutation, UpdatePostMutationVariables>(UpdatePostDocument);
 };
 export const VoteDocument = gql`
     mutation Vote($value: Int!, $postId: Int!) {
